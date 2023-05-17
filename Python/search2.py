ba78a3c714d362965908144911325c3be1b31386
ayan878 <ayanraza197@gmail.com>
2023-05-18 01:01:37 +0530

    cat II

---
+++ Python/search2.py
@@ -0,0 +1,38 @@
+
+#linear Search
+def linearSearch(arr,target):
+    for i in range(len(arr)):
+        if arr[i]== target:
+            return i
+    return -1
+
+#binary search
+def binarySearch(arr,target):
+    low=0
+    high=len(arr)-1
+    while low <= high:
+        mid=low+high//2
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+        
+    return -1
+arr=[1,2,3,4,5,6,7,8,9]
+target=4
+
+linear_index= linearSearch(arr,target)
+if linear_index !=-1:
+    print("Linear search:Element",target,"at index",linear_index)
+else:
+    print("Linear search:Element",target,"not found")
+
+binary_index= linearSearch(arr,target)
+if linear_index !=-1:
+    print("Linear search:Element",target,"at index",binary_index)
+else:
+    print("Linear search:Element",target,"not found")
+
+
